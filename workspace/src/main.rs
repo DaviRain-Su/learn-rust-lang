01d4ce026e2123297219f952acf417ad90d48835
Davirain <davirain.yin@gmail.com>
2022-06-26 03:22:37 +0800

    add rust-101

---
+++ workspace/src/main.rs
@@ -0,0 +1,30 @@
+#![allow(dead_code, unused_imports, unused_variables, unused_mut, unreachable_code)]
+
+// To get started with the course, open the file `part00.rs` in this workspace as well
+// as its [fully explained version](https://www.ralfj.de/projects/rust-101/part00.html).
+// The code below is mostly boilerplate to dispatch into the various parts of the
+// tutorial.
+
+// Only the files imported here will be compiled.
+mod part00;
+mod part01;
+mod part02;
+mod part03;
+mod part04;
+mod part05;
+mod part06;
+mod part07;
+mod part08;
+mod part09;
+mod part10;
+mod part11;
+mod part12;
+mod part13;
+mod part14;
+mod part15;
+mod part16;
+
+// This decides which part is actually run.
+fn main() {
+   part00::main();
+}
