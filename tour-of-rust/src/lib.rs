67aabf04be4a7645fa1dbf9ed4fb0d30278058cf
Davirain <davirain.yin@gmail.com>
2022-06-26 12:37:29 +0800

    Update lib.rs

--- tour-of-rust/src/lib.rs
+++ tour-of-rust/src/lib.rs
@@ -1,10 +1,20 @@
+/// # Chapter 1 The Basic
 pub mod chapter1_the_basic;
+/// # Chapter 2 Basic Control Flow
 pub mod chapter2_basic_control_flow;
+/// # Chapter 3 Basic Data Struct Types
 pub mod chapter3_basic_data_struct_types;
+/// # Chapter 4 Generic Types
 pub mod chapter4_generic_type;
+/// # Chapter 5 Ownership And Borrowing Data
 pub mod chapter5_ownership_borrowing_data;
+/// # Chapter 6 Text
 pub mod chapter6_text;
+/// # Chapter 7  Object Oriented Programming
 pub mod chapter7_object_oriented_programming;
+/// # Chapter 8 Smart Pointer
 pub mod chapter8_smart_pointer;
+/// # Chapter 9 Project Organization  and Structure
 pub mod chapter9_project_organization_and_structure;
+/// # Chapter 10 The End
 pub mod chapter10_the_end;
