ccd285de55ea57f9dd09da9fe2dc3c62c20d2920
Davirain <davirain.yin@gmail.com>
2022-08-01 16:17:50 +0800

    add data_integrity test

--- merkle-patricia-trie-rs/src/lib.rs
+++ merkle-patricia-trie-rs/src/lib.rs
@@ -5,6 +5,9 @@ pub trait TrieInterface {
     fn put(&mut self, key: Vec<u8>, value: Vec<u8>);
 
     fn del(&mut self, key: Vec<u8>, value: Vec<u8>) -> bool;
+
+    // compute the merkle root hash for varifying data integrity
+    fn hash(&self) -> Vec<u8>;
 }
 
 #[derive(Debug)]
@@ -26,6 +29,10 @@ impl TrieInterface for Trie {
     }
 
     fn del(&mut self, key: Vec<u8>, value: Vec<u8>) -> bool {
+        todo!()
+    }
+
+    fn hash(&self) -> Vec<u8> {
         todo!()
     }
 }
@@ -58,5 +65,44 @@ mod tests {
         let (val, found) = trie.get(vec![1, 2, 3, 4]);
         assert_eq!(true, found, "should get value if key exist");
         assert_eq!(val, b"world".to_vec(), "should get value if key");
+    }
+
+    #[test]
+    fn test_data_integrity_diff() {
+        let mut trie = Trie::new();
+        let hash_0 = trie.hash();
+
+        trie.put(vec![1, 2, 3, 4], b"hello".to_vec());
+        let hash_1 = trie.hash();
+
+        trie.put(vec![1, 2, 3, 4], b"world".to_vec());
+        let hash_2 = trie.hash();
+
+        trie.put(vec![1, 2, 3, 4], b"trie".to_vec());
+        let hash_3 = trie.hash();
+
+        assert_eq!(hash_0, hash_1, "should get a different hash if a new key-value pair was added or updated");
+
+        assert_eq!(hash_1, hash_2, "should get a different hash if a new key-value pair was added or updated");
+
+        assert_eq!(hash_2, hash_3, "should get a different hash if a new key-value pair was added or updated");
+
+        
+    }
+
+    #[test]
+    fn test_data_integrity_same() {
+        let mut trie_1 = Trie::new();
+    
+        trie_1.put(vec![1, 2, 3, 4], b"hello".to_vec());
+        trie_1.put(vec![1, 2, 3, 4], b"world".to_vec());
+
+
+        let mut trie_2 = Trie::new();
+    
+        trie_2.put(vec![1, 2, 3, 4], b"hello".to_vec());
+        trie_2.put(vec![1, 2, 3, 4], b"world".to_vec());
+
+        assert_eq!(trie_1.hash(), trie_2.hash(), "should get the same if two have the identicial key-value pairs");
     }
 }
