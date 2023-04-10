31ef81175cd386d26f2c21d27d85a510f02c6aa2
DaviRin <davirain.yin@gmail.com>
2023-04-10 14:24:16 +0800

    add csapp-rs

--- csapp/main.cpp
+++ csapp/main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
-int main() { std::cout << "Hello from a C++ Buck2 program!" << std::endl; }
+#include <stdio.h>
+
+int main() {
+  std::cout << "Hello from a C++ Buck2 program!" << std::endl;
+  printf("hello, world! From C program\n");
+  printf("(500*400)*(300*200) = %d\n", (500 * 400) * (300 * 200));
+  printf("((500*400)*300)*200) = %d\n", ((500 * 400) * 300) * 200);
+  printf("((200*500)*300)*400) = %d\n", ((200 * 500) * 300) * 400);
+  printf("400*(200 *(300*500)) = %d\n", 400 * (200 * (300 * 500)));
+
+  printf("(3.14+1e20) - 1e20 = %f\n", (3.14 + 1e20) - 1e20);
+}
